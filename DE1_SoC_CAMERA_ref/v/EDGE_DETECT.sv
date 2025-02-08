cf7d7933c746c0dd6ef0f4aa99e0caf9e5a5607c
hdoll2 <hdoll2@wisc.edu>
2025-02-08 15:14:54 -0600

    greyscale

--- DE1_SoC_CAMERA_ref/v/EDGE_DETECT.sv
+++ DE1_SoC_CAMERA_ref/v/EDGE_DETECT.sv
@@ -67,25 +67,38 @@ output	[11:0]	oRed;
 output	[11:0]	oGreen;
 output	[11:0]	oBlue;
 output			oDVAL;
-wire	[11:0]	mDATA_0;
-wire	[11:0]	mDATA_1;
-reg		[11:0]	mDATAd_0;
-reg		[11:0]	mDATAd_1;
+//wire	[11:0]	mDATA_0;
+//wire	[11:0]	mDATA_1;
+//reg		[11:0]	mDATAd_0;
+//reg		[11:0]	mDATAd_1;
+wire	[11:0]	mDATA;
+reg		[11:0]	mDaTAd;
 reg		[11:0]	mCCD_R;
 reg		[12:0]	mCCD_G;
 reg		[11:0]	mCCD_B;
 reg				mDVAL;
+
 
 assign	oRed	=	((iX_Cont < 10 && iY_Cont == 1)) ? '1 : mCCD_R[11:0];
 assign	oGreen	=	((iX_Cont < 10 && iY_Cont == 1)) ? '0 : mCCD_G[12:1];
 assign	oBlue	=	((iX_Cont < 10 && iY_Cont == 1)) ? '0 : mCCD_B[11:0];
 assign	oDVAL	=	mDVAL;
 
+/*
 Line_Buffer1 	u0	(	.clken(iDVAL),
 						.clock(iCLK),
 						.shiftin(iDATA),
 						.taps0x(mDATA_1),
-						.taps1x(mDATA_0)	);
+						.taps1x(mDATA_0)	)
+*/
+Line_Buffer2 u0 (
+	//Inputs
+	.clken(iDVAL),
+	.clock(iCLK),
+	.shiftin(iDATA),
+	.taps(mDATA)
+);						
+
 
 always@(posedge iCLK or negedge iRST)
 begin
@@ -94,11 +107,13 @@ begin
 		mCCD_R	<=	0;
 		mCCD_G	<=	0;
 		mCCD_B	<=	0;
-		mDATAd_0<=	0;
-		mDATAd_1<=	0;
+		//mDATAd_0<=	0;
+		//mDATAd_1<=	0;
 		mDVAL	<=	0;
+		mDATAd 	<= 	0;
 	end
 	else
+	/*
 	begin
 		mDATAd_0	<=	mDATA_0;
 		mDATAd_1	<=	mDATA_1;
@@ -128,7 +143,21 @@ begin
 			mCCD_B	<=	mDATA_0;
 		end
 	end
+	*/
+	begin
+		mDATA_ff <= mDATA;
+		iDATA_ff <= iDATA;
+		mDVAL <= {iY_Cont[0]|iX_Cont[0]} ? 1'b0 : iDVAL;
+
+		// Greyscale conversion
+		
+
+		mCCD_R <=
+		mCCD_G <=
+		mCCD_B <=
+	end
 end
+
 
 endmodule
 
